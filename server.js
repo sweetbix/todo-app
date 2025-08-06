38edd234545afa47d6948651b9d8f2fe3a5e901c
Richard <46233781+sweetbix@users.noreply.github.com>
2025-08-06 22:37:58 +1000

    fixes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
@@ -22,8 +23,8 @@ app.use("/api/auth", authRoutes);
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("Connected to MongoDB!");
-        app.listen(PORT, () => console.log(`Server running on ${PORT}`));
     }) 
     .catch(err => console.error("MongoDB connection error:", err));
 
+// export for vercel
 module.exports = app;
