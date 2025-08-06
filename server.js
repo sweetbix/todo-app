decd4de1c97ab027749e15426e75f7ecc658ec42
Richard <46233781+sweetbix@users.noreply.github.com>
2025-08-06 22:32:02 +1000

    changes for deployment

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const todoRoutes = require("./routes/todoRoutes");
-const authRoutes = require("./routes/authRoutes");
-const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
@@ -13,9 +10,12 @@ const app = express();
 app.use(cookieParser())
 app.use(express.json());
 app.use(cors({
-    origin: "https://richardguotodo.vercel.app/"
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true
 }));
 
+const todoRoutes = require("./routes/todoRoutes");
+const authRoutes = require("./routes/authRoutes");
 app.use("/api/todos", todoRoutes);
 app.use("/api/auth", authRoutes);
 
@@ -25,3 +25,5 @@ mongoose.connect(process.env.MONGO_URI)
         app.listen(PORT, () => console.log(`Server running on ${PORT}`));
     }) 
     .catch(err => console.error("MongoDB connection error:", err));
+
+module.exports = app;
